bc09bbf16e6f15497a638b75030d2262b4edbbd4
ervandogan12 <ervandogan12@gmail.com>
2024-06-23 10:30:19 +0200

    port modified

--- hyf-react-project/backend/server.js
+++ hyf-react-project/backend/server.js
@@ -38,5 +38,4 @@ app.post('/login', async (req, res) => {
   res.json({ token });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(10000, () => console.log('Server running on port 10000'));
