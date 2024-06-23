1553d6068c26c307d8d3a7c4f04f22879fd2b319
ervandogan12 <ervandogan12@gmail.com>
2024-06-23 12:11:54 +0200

    back end config

--- hyf-react-project/src/Login.jsx
+++ hyf-react-project/src/Login.jsx
@@ -1,48 +1,60 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from './context/AuthContext';
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "./context/AuthContext";
 
 const AuthForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
   const navigate = useNavigate();
   const { login } = useAuth();
- 
-  const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || 'https://react-project-2-3m1d.onrender.com';
+
+  const API_BASE_URL =
+    import.meta.env.VITE_REACT_APP_BACKEND_URL ||
+    "https://react-project-2-3m1d.onrender.com";
+  // const API_BASE_URL = import.meta.env.VITE_REACT_APP_BACKEND_URL || 'http://localhost:10000';
+  console.log(API_BASE_URL);
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`${API_BASE_URL}/login`, { email, password });
+      const response = await axios.post(`${API_BASE_URL}/register`, {
+        email,
+        password,
+      });
+
       login(response.data.token);
-     
-      setEmail('');
-      setPassword('');
-      navigate('/');
+      setMessage("Registration successful");
+      setEmail("");
+      setPassword("");
+      navigate("/");
     } catch (error) {
-      setMessage(error.response.data.error); // Assuming the error response has an error field
+      setMessage(error.response.data.error);
     }
   };
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('https://react-project-2-3m1d.onrender.com/login', { email, password });
+      const response = await axios.post(`${API_BASE_URL}/login`, {
+        email,
+        password,
+      });
+
       login(response.data.token);
-      setMessage('Login successful');
-     
-      setEmail('');
-      setPassword('');
-      navigate('/');
+      setMessage("Login successful");
+
+      setEmail("");
+      setPassword("");
+      navigate("/");
     } catch (error) {
       setMessage(error.response.data.error); // Assuming the error response has an error field
     }
   };
 
   return (
-    <div className='authform-container'>
-      <form className='auth-form'>
+    <div className="authform-container">
+      <form className="auth-form">
         <label htmlFor="email">Email</label>
         <input
           id="email"
@@ -61,8 +73,12 @@ const AuthForm = () => {
           placeholder="Password"
           required
         />
-        <button className="auth-button" onClick={handleRegister}>Register</button>
-        <button className="auth-button" onClick={handleLogin}>Login</button>
+        <button className="auth-button" onClick={handleRegister}>
+          Register
+        </button>
+        <button className="auth-button" onClick={handleLogin}>
+          Login
+        </button>
         {message && <p>{message}</p>}
       </form>
     </div>
