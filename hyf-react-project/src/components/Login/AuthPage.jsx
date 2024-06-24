11ac4ba300639338aa0989b24460fc606f212939
ervandogan12 <ervandogan12@gmail.com>
2024-06-24 13:08:27 +0200

    circle indicator added

--- hyf-react-project/src/components/Login/AuthPage.jsx
+++ hyf-react-project/src/components/Login/AuthPage.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AuthForm from './AuthForm';
 
 function AuthPage() {
+ 
+  const [isLoading, setIsLoading] = useState(false);
+
+ 
+  const handleLogin = async (loginDetails) => {
+    setIsLoading(true);
+   
+    await new Promise((resolve) => setTimeout(resolve, 2000)); 
+    setIsLoading(false);
+  };
+
   return (
     <div>
       <h1>Welcome</h1>
       <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-        <AuthForm />
+        {isLoading ? (
+          <div className="spinner" style={{ display: 'flex', justifyContent: 'center' }}>
+            <div className="lds-ring">
+              <div></div><div></div><div></div><div></div>
+            </div>
+          </div>
+        ) : (
+          <AuthForm onLogin={handleLogin} />
+        )}
       </div>
     </div>
   );
 }
 
 export default AuthPage;
+
