fecc064095e641efd2f5d817a240ebb9fdb647b3
AhmetViamanta <ahmet.dogan@viamanta.com>
2026-05-22 10:40:25 +0200

    ffffffffff

---
+++ hyf-react-project/src/components/Login/AuthForm.jsx
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+
+const AuthForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+  const { login } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const API_BASE_URL =import.meta.env.VITE_REACT_APP_BACKEND_URL ||"https://react-project-1-x96e.onrender.com";
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const response = await axios.post(`${API_BASE_URL}/register`, {
+        email,
+        password,
+      });
+  
+      login(response.data.token, email);
+      setMessage("Registration successful");
+      setEmail("");
+      setPassword("");
+      navigate("/");
+    } catch (error) {
+      setMessage(error.response.data.error);
+    }finally {
+      setIsLoading(false); 
+    }
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);     
+    try {
+      const response = await axios.post(`${API_BASE_URL}/login`, {
+        email,
+        password,
+      });   
+      login(response.data.token, email);
+      setMessage("Login successful");
+      setEmail("");
+      setPassword("");
+      navigate("/");
+    } catch (error) {
+      setMessage(error.response.data.error);
+      console.error("Login error:", error);
+      setMessage("Login failed");
+    } finally {
+      setIsLoading(false); 
+    }
+  //lagin handler 
+
+  // const handleLogin = async (e) => {
+  //   e.preventDefault();
+  //   setIsLoading(true);
+  //   try {
+  //     const response = await axios.post(`${API_BASE_URL}/login`, {
+  //       email,
+  //       password,
+  //     });
+
+  //     login(response.data.token, email);
+  //     setMessage("Login successful");
+  //     setEmail("");
+  //     setPassword("");
+  //     navigate("/");
+  //   } catch (error) {
+  //     setMessage(error.response.data.error);
+  //     console.error("Login error:", error);
+  //     setMessage("Login failed");
+  //     setIsLoading(false);
+  //   }
+  // };
+
+
+
+  return (
+    <div className="authform-container">
+      <form className="auth-form">
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        {isLoading ? (
+          <div className="loading-spinner"></div> 
+        ) : (
+          <>
+            <button className="auth-button" onClick={handleRegister}>
+              Register
+            </button>
+            <button className="auth-button" onClick={handleLogin}>
+              Login
+            </button>
+          </>
+        )}
+        {message && <p>{message}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default AuthForm;
